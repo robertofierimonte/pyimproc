14343de6d7ce846356f8372eb3586bee8dbffb85
Roberto Fierimonte <roberto.fierimonte@gmail.com>
2026-06-27 22:11:56 +0300

    feat: Added more colour space analyses and notebook for webcam inputs
    Signed-off-by: Roberto Fierimonte <roberto.fierimonte@gmail.com>

--- src/pyimproc/analysis.py
+++ src/pyimproc/analysis.py
@@ -1,19 +1,27 @@
+from typing import Literal
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def histogram_plot(image: np.ndarray) -> None:
+def histogram_plot(image: np.ndarray, mode: Literal["line", "hist"], norm: bool = False) -> None:
     """Plot the histogram of the image."""
     plt.figure(figsize=(10, 5))
     plt.title("Histogram of Pixel Intensities")
     plt.xlabel("Pixel Intensity")
     plt.ylabel("Frequency")
     if len(image.shape) == 2:  # Grayscale image
-        plt.hist(image.ravel(), bins=256, range=(0, 255), color='black')
+        if mode == "hist":
+            plt.hist(image.ravel(), bins=256, range=(0, 255), color='black')
+        else:
+            plt.plot(np.arange(256), np.histogram(image.ravel(), bins=256, range=(0, 255))[0], color='black')
     else:  # Color image
         colors = ("r", "g", "b")
         for i, color in enumerate(colors):
-            plt.hist(image[:, :, i].ravel(), bins=256, range=(0, 255), color=color, alpha=0.5, label=f'{color.upper()} channel')
+            if mode == "hist":
+                plt.hist(image[:, :, i].ravel(), bins=256, range=(0, 255), color=color, alpha=0.5, label=f'{color.upper()} channel')
+            else:
+                plt.plot(np.arange(256), np.histogram(image[:, :, i].ravel(), bins=256, range=(0, 255))[0], color=color, alpha=0.5, label=f'{color.upper()} channel')
     plt.xlim([0, 255])
     plt.legend()
     plt.show()
